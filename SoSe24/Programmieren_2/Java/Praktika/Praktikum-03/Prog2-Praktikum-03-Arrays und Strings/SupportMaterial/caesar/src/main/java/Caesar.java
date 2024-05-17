43df7e8a237ce2fde4039f4112d09517f63389b6
Korbiniannn <korbi19.4@gmail.com>
2024-05-17 20:11:40 +0200

    progress in projekt_03

---
+++ SoSe24/Programmieren_2/Java/Praktika/Praktikum-03/Prog2-Praktikum-03-Arrays und Strings/SupportMaterial/caesar/src/main/java/Caesar.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Caesar{
+
+  public final static String ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  public final static String EXAMPLE_KEY = "HAMNOGPQFEISTLJVWDXKCYZRBU";
+
+  public static String applyCaesar(String text, String key, String clear){
+    int n = text.length();
+    StringBuilder result = new StringBuilder();
+    for(int i = 0; i < n; i++){
+      for (int j = 0; j < clear.length(); j++){
+        if (text.charAt(i) == key.charAt(j)){
+          result.append(clear.charAt(j));
+        }
+      }
+      if(!key.contains(String.valueOf(text.charAt(i)))){
+        result.append(text.charAt(i)); 
+      }
+    }
+    return result.toString();
+  }
+
+  public static void printStatistics(String text){
+    // TODO
+  }
+
+  public static void main(String[] args){
+    String message = "PROGRAMMIEREN IN JAVA MACHT NOCH MEHR SPASS ALS IN PYTHON ODER C";
+
+    String encrypted = applyCaesar(message, EXAMPLE_KEY, ABC);
+    String decrypted = applyCaesar(encrypted, ABC, EXAMPLE_KEY);
+
+    System.out.printf("Message: %s%n", message);
+    System.out.printf("Encrypted: %s%n", encrypted);
+    System.out.printf("Decrypted: %s%n", decrypted);
+
+    printStatistics(encrypted);
+  }
+
+  public final static String EXAM_HINTS_ENCRYPTED = "WJ EG ULT SPDWFWT RW VLFKLNLG JÜFFLG FEL GWT HAPBLGUL NEGZLEFL VLDQNKLG:\r\n"+
+" - UEL SPDWFWT ATELGKELTK FEQN DG ULG CTDSKESD!\r\n"+
+" - VLDTVLEKLG FEL DPPL CTDSKESWJFDWHBDVLG FLPVFKFKÄGUEB. GWT FA OLTFKLNLG FEL UEL PÖFWGBLG WGU SÖGGLG DWQN EG ULT SPDWFWT FLPVFKFKÄGUEB DWH UEL PÖFWGB SAJJLG.\r\n"+
+" - FLNLG FEL ULG PLEFKWGBFGDQNZLEF DPF LEGL BLPLBLGNLEK LEGL TÜQSJLPUWGB RW ENTLJ ZEFFLGFFKDGU RW VLSA JJLG. ZLGG FEL LEGJDP LEG CTDSKESWJ GEQNK VLFKLNLG, UDGG GWKRLG FEL UEL BLPLBLGNLEK WJ FEQN KELHLT EGUEL JDKLTEL LEGRWDTVLEKLG.\r\n"+
+" - FLNLG FEL FEQN DPKL SPDWFWTLG DG WGU TLQNGLG FEL UELFL UWTQN. UEL SPDWFWTLG EG CTABTDJJELTLG 2 FEGU EJJLT ÄNGPEQN DWHBLVDWK. FEL UÜTHLG FEQN FABDT UEL DPKLG SPDWFWTLG JEK ENTLG PÖFWGBLG JEK EG UEL CTÜH WGB GLNJLG.\r\n"+
+" - CTAVELTLG FEL UEL VLEFCELPL DWF ULT OATPLFWGB FLPVLT DWF, OLTÄGULTG FEL FEL, JDQNLG FEL FEL SDCWKK WGU TLCDTELTLG FEL FEL. FA VLSAJJLG FEL LEG BLHÜNP HÜT UEL CTABTDJJELTFCTDQNL.\r\n"+
+"- ZLGG UDF DPPLF GEQNKF NEPHK, UDGG HEGULG FEL NELT UEL DWHBDVLG ULT GÄQNFKLG CTABTDJJJELTLG 2 SPDWFWT FDJK PÖFWGBLG: NKKCF://VEK.PY/CTAB2-SPDWFWT-FAFL24";
+
+
+
+
+
+
+
+}
